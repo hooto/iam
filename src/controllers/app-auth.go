d93c36b556eb5bfd3193f8dfeec319a8c4dc8b2f
Eryx <evorui@gmail.com>
2014-06-22 23:39:14 +0800

    [NEW] AppAuth/Register

--- src/controllers/app-auth.go
+++ src/controllers/app-auth.go
@@ -4,14 +4,60 @@ import (
     "../../deps/lessgo/data/rdo"
     "../../deps/lessgo/data/rdo/base"
     "../../deps/lessgo/pagelet"
-    "../../deps/lessgo/pass"
     "../../deps/lessgo/utils"
     "io"
-    "strings"
 )
 
 type AppAuth struct {
     *pagelet.Controller
+}
+
+func (c AppAuth) InfoAction() {
+
+    c.AutoRender = false
+
+    var rsp struct {
+        ResponseJson
+        Data struct {
+            InstanceId string `json:"instance_id"`
+            AppId      string `json:"app_id"`
+            Version    string `json:"version"`
+        }   `json:"data"`
+    }
+    rsp.ApiVersion = apiVersion
+    rsp.Status = 400
+    rsp.Message = "Bad Request"
+
+    defer func() {
+        if rspj, err := utils.JsonEncode(rsp); err == nil {
+            io.WriteString(c.Response.Out, rspj)
+        }
+    }()
+
+    dcn, err := rdo.ClientPull("def")
+    if err != nil {
+        rsp.Status = 500
+        rsp.Message = "Internal Server Error"
+        return
+    }
+
+    instanceid := c.Params.Get("instanceid")
+
+    q := base.NewQuerySet().From("ids_instance").Limit(1)
+    q.Where.And("id", instanceid)
+    rs, err := dcn.Base.Query(q)
+    if err == nil && len(rs) == 0 {
+        rsp.Status = 404
+        rsp.Message = "Instance Not Found"
+        return
+    }
+
+    rsp.Data.InstanceId = rs[0].Field("id").String()
+    rsp.Data.AppId = rs[0].Field("app_id").String()
+    rsp.Data.Version = rs[0].Field("version").String()
+
+    rsp.Status = 200
+    rsp.Message = ""
 }
 
 func (c AppAuth) RegisterAction() {
@@ -25,6 +71,7 @@ func (c AppAuth) RegisterAction() {
             AppId      string `json:"app_id"`
             AppTitle   string `json:"app_title"`
             Version    string `json:"version"`
+            Url        string `json:"url"`
             Privileges []struct {
                 Key  string `json:"key"`
                 Desc string `json:"desc"`
@@ -37,13 +84,40 @@ func (c AppAuth) RegisterAction() {
     rsp.Status = 400
     rsp.Message = "Bad Request"
 
-    rsp.Data.Continue = "/ids"
-
     defer func() {
         if rspj, err := utils.JsonEncode(rsp); err == nil {
             io.WriteString(c.Response.Out, rspj)
         }
     }()
+
+    body := c.Request.RawBodyString()
+    if body == "" {
+        return
+    }
+
+    var req struct {
+        AccessToken string `json:"access_token"`
+        Data        struct {
+            InstanceId  string `json:"instance_id"`
+            InstanceUrl string `json:"instance_url"`
+            AppId       string `json:"app_id"`
+            AppTitle    string `json:"app_title"`
+            Version     string `json:"version"`
+            Privileges  []struct {
+                Key  string `json:"key"`
+                Desc string `json:"desc"`
+            }   `json:"privileges"`
+        }   `json:"data"`
+    }
+
+    err := utils.JsonDecode(body, &req)
+    if err != nil {
+        rsp.Message = err.Error()
+        return
+    }
+    if req.AccessToken == "" {
+        return
+    }
 
     dcn, err := rdo.ClientPull("def")
     if err != nil {
@@ -51,61 +125,112 @@ func (c AppAuth) RegisterAction() {
         return
     }
 
-    if c.Params.Get("email") == "" || c.Params.Get("passwd") == "" {
+    if !c.Session.AccessAllowed("sys.admin") {
+        rsp.Status = 401
+        rsp.Message = "Unauthorized"
         return
     }
 
-    email := strings.ToLower(c.Params.Get("email"))
+    sess, err := c.Session.SessionFetch()
 
-    q := base.NewQuerySet().From("ids_login").Limit(1)
-    q.Where.And("email", email)
-    rsu, err := dcn.Base.Query(q)
-    if err == nil && len(rsu) == 0 {
-        rsp.Message = "Email or Password can not match"
+    q := base.NewQuerySet().From("ids_instance").Limit(1)
+    q.Where.And("id", req.Data.InstanceId)
+    rs, err := dcn.Base.Query(q)
+    if err != nil {
+        rsp.Message = "Internal Server Error"
         return
     }
+    if len(rs) == 0 {
 
-    if !pass.Check(c.Params.Get("passwd"), rsu[0].Field("pass").String()) {
-        rsp.Message = "Email or Password can not match"
-        return
-    }
-
-    rsp.Data.AccessToken = utils.StringNewRand36(24)
-
-    addr := "127.0.0.1"
-    if addridx := strings.Index(c.Request.RemoteAddr, ":"); addridx > 0 {
-        addr = c.Request.RemoteAddr[:addridx]
-    }
-    //fmt.Println(c.Request.RemoteAddr, addr, c.Request.Request)
-
-    session := map[string]interface{}{
-        "token":    rsp.Data.AccessToken,
-        "refresh":  utils.StringNewRand36(24),
-        "status":   1,
-        "uid":      rsu[0].Field("uid").Int(),
-        "uuid":     rsu[0].Field("uuid").String(),
-        "uname":    rsu[0].Field("uname").String(),
-        "name":     rsu[0].Field("name").String(),
-        "roles":    rsu[0].Field("roles").String(),
-        "timezone": rsu[0].Field("timezone").String(),
-        "source":   addr,
-        "created":  base.TimeNow("datetime"),                // TODO
-        "expired":  base.TimeNowAdd("datetime", "+864000s"), // TODO
-    }
-    if _, err := dcn.Base.Insert("ids_sessions", session); err != nil {
-        rsp.Status = 500
-        rsp.Message = "Can not write to database" + err.Error()
-        return
-    }
-
-    if len(c.Params.Get("continue")) > 0 {
-        rsp.Data.Continue = c.Params.Get("continue")
-        if strings.Index(rsp.Data.Continue, "?") == -1 {
-            rsp.Data.Continue += "?"
-        } else {
-            rsp.Data.Continue += "&"
+        item := map[string]interface{}{
+            "id":        req.Data.InstanceId,
+            "uid":       sess.Uid,
+            "status":    1,
+            "app_id":    req.Data.AppId,
+            "app_title": req.Data.AppTitle,
+            "version":   req.Data.Version,
+            "url":       req.Data.InstanceUrl,
+            "created":   base.TimeNow("datetime"),
+            "updated":   base.TimeNow("datetime"),
         }
-        rsp.Data.Continue += "access_token=" + rsp.Data.AccessToken
+
+        if _, err := dcn.Base.Insert("ids_instance", item); err != nil {
+            rsp.Status = 500
+            rsp.Message = "Can not write to database" + err.Error()
+            return
+        }
+
+    } else {
+
+        item := map[string]interface{}{
+            "app_title": req.Data.AppTitle,
+            "version":   req.Data.Version,
+            "url":       req.Data.InstanceUrl,
+            "updated":   base.TimeNow("datetime"),
+        }
+        frupd := base.NewFilter()
+        frupd.And("id", req.Data.InstanceId)
+        if _, err := dcn.Base.Update("ids_instance", item, frupd); err != nil {
+            rsp.Status = 500
+            rsp.Message = "Can not write to database" + err.Error()
+            return
+        }
+    }
+
+    //
+    q = base.NewQuerySet().From("ids_privilege").Limit(1000)
+    q.Where.And("instance", req.Data.InstanceId)
+    rs, err = dcn.Base.Query(q)
+    if err != nil {
+        rsp.Message = "Internal Server Error"
+        return
+    }
+
+    for _, prePriv := range rs {
+
+        isExist := false
+        for _, curPrev := range req.Data.Privileges {
+
+            if prePriv.Field("privilege").String() == curPrev.Key {
+                isExist = true
+                break
+            }
+        }
+
+        if !isExist {
+            frupd := base.NewFilter()
+            frupd.And("instance", req.Data.InstanceId).And("privilege", prePriv.Field("privilege").String())
+            dcn.Base.Delete("ids_privilege", frupd)
+        }
+    }
+
+    for _, curPrev := range req.Data.Privileges {
+
+        isExist := false
+
+        for _, prePriv := range rs {
+
+            if prePriv.Field("privilege").String() == curPrev.Key {
+                isExist = true
+                break
+            }
+        }
+
+        if !isExist {
+            item := map[string]interface{}{
+                "instance":  req.Data.InstanceId,
+                "uid":       sess.Uid,
+                "privilege": curPrev.Key,
+                "desc":      curPrev.Desc,
+                "created":   base.TimeNow("datetime"),
+            }
+
+            if _, err := dcn.Base.Insert("ids_privilege", item); err != nil {
+                rsp.Status = 500
+                rsp.Message = "Can not write to database" + err.Error()
+                return
+            }
+        }
     }
 
     rsp.Status = 200
